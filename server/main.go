a6accfeffbaa8551605ff7cd3ad17022b719341f
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-03-18 22:50:06 +0900

    modified go-server

--- server/main.go
+++ server/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello")
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{})
+	})
+
+	router.Run(":3000")
 }
