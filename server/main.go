7bb73b7801758e1d8d2dbf88cf45b31bc4f2a2dc
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-03-24 23:05:09 +0900

    added config codes for CORS

--- server/main.go
+++ server/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"time"
+
 	"github.com/be3/go_vue_todo/server/controller"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+
+	router.Use(Cors()) // CORSの設定
 
 	router.GET("/list", controller.TaskList)
 	router.POST("/create", controller.CreateTask)
@@ -15,4 +20,34 @@ func main() {
 	router.DELETE("/delete/:id", controller.DeleteTask)
 
 	router.Run(":3000")
+}
+
+func Cors() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		// アクセスを許可したいアクセス元
+		AllowOrigins: []string{
+			"http://localhost:8080",
+		},
+		// アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
+		AllowMethods: []string{
+			"GET",
+			"POST",
+			"PUT",
+			"DELETE",
+			"OPTIONS",
+		},
+		// 許可したいHTTPリクエストヘッダ
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+		// cookieなどの情報を必要とするかどうか
+		AllowCredentials: true,
+		// preflightリクエストの結果をキャッシュする時間
+		MaxAge: 24 * time.Hour,
+	})
 }
