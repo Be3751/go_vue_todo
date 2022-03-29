5ffe32abe0c8c150e5e0142bee160c6ea0568f69
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-03-30 00:18:19 +0900

    Added buttons for view task list and create task

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,8 +1,16 @@
 <template>
   <v-app-bar absolute color="black" dark app>
-      <v-toolbar-title>TASK MANAGEMENT</v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-app-bar>
+    <v-toolbar-title>Task Management!</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn class="ma-2" color="teal accent-3">
+      <v-icon dark>mdi-format-list-bulleted-square</v-icon>
+      View task list
+    </v-btn>
+    <v-btn class="ma-2" color="teal accent-3">
+      <v-icon dark>mdi-pencil</v-icon>
+      Create task
+    </v-btn>
+  </v-app-bar>
 </template>
 
 <script>
