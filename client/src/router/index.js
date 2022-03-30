bdbaee5511b0141969309279c5301d6b7dd556ba
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-03-31 00:55:15 +0900

    updated setting for routing

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,22 +1,38 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import TaskList from '@/views/TaskList.vue'
+import CreateTask from '@/views/CreateTask.vue'
+import ReadTask from '@/views/ReadTask.vue'
+import UpdateTask from '@/views/UpdateTask.vue'
+import DeleteTask from '@/views/DeleteTask.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'list',
+    component: TaskList
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/create',
+    name: 'create',
+    component: CreateTask
+  },
+  {
+    path: '/read',
+    name: 'read',
+    component: ReadTask 
+  },
+  {
+    path: '/update',
+    name: 'update',
+    component: UpdateTask 
+  },
+  {
+    path: '/delete',
+    name: 'delete',
+    component: DeleteTask 
   }
 ]
 
