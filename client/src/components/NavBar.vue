e15a77651f6bea2ce3c4f01a7593dd4abe696774
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-03-31 13:16:36 +0900

    added router and routes

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -2,11 +2,11 @@
   <v-app-bar absolute color="black" dark app>
     <v-toolbar-title>Task Management!</v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-btn class="ma-2" color="teal accent-3">
+    <v-btn to="/" class="ma-2" color="teal accent-3">
       <v-icon dark>mdi-format-list-bulleted-square</v-icon>
       View task list
     </v-btn>
-    <v-btn class="ma-2" color="teal accent-3">
+    <v-btn to="/create" class="ma-2" color="teal accent-3">
       <v-icon dark>mdi-pencil</v-icon>
       Create task
     </v-btn>
--- client/src/components/TaskCard.vue
+++ client/src/components/TaskCard.vue
@@ -2,9 +2,9 @@
     <v-card class="mt-5 mb-5 ml-2 mr-2" style="width: 30%;">
         <v-card-title>{{ task.content }}</v-card-title>
         <v-card-actions>
-            <v-btn outlined rounded text color="orange">Detail</v-btn>
-            <v-btn outlined rounded text color="green">Update</v-btn>
-            <v-btn outlined rounded text color="red">Delete</v-btn>
+            <v-btn to="/read" outlined rounded text color="orange">Detail</v-btn>
+            <v-btn to="/update" outlined rounded text color="green">Update</v-btn>
+            <v-btn to="/delete" outlined rounded text color="red">Delete</v-btn>
         </v-card-actions>
     </v-card>
 </template>
