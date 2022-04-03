ab5842a7fb6b5d20ab90b54cd8d775279f524762
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-04-03 20:41:40 +0900

    added page and function to read task #7

--- client/src/components/TaskCard.vue
+++ client/src/components/TaskCard.vue
@@ -2,7 +2,7 @@
     <v-card class="mt-5 mb-5 ml-2 mr-2" style="width: 30%;">
         <v-card-title>{{ task.content }}</v-card-title>
         <v-card-actions>
-            <v-btn to="/read" outlined rounded text color="orange">Detail</v-btn>
+            <v-btn v-on:click="toRead" outlined rounded text color="orange">Detail</v-btn>
             <v-btn v-on:click="toUpdate" outlined rounded text color="green">Update</v-btn>
             <v-btn v-on:click="toDelete" outlined rounded text color="red">Delete</v-btn>
         </v-card-actions>
@@ -19,6 +19,9 @@ export default {
         }
     },
     methods: {
+        toRead() {
+            this.$router.push({name: "read", params: {id: this.task.id, content: this.task.content}}) // パラメータidを指定してupdate/:idに遷移
+        },
         toUpdate() {
             this.$router.push({name: "update", params: {id: this.task.id}}) // パラメータidを指定してupdate/:idに遷移
         },
