8cc5ed1be175e7df105669c1cf0c24088d3af343
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-04-10 20:46:26 +0900

    added method to create session #17

--- server/service/user.go
+++ server/service/user.go
@@ -3,9 +3,11 @@ package service
 import (
 	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/be3/go_vue_todo/server/crypto"
 	"github.com/be3/go_vue_todo/server/model"
+	"github.com/google/uuid"
 )
 
 type UserService struct{}
@@ -16,7 +18,7 @@ func (UserService) AddUser(id string, pwd string) error {
 	// 既にユーザが登録済みかを確認
 	var user model.User
 	_ = Db.QueryRow("select id from users where id = ?", id).Scan(&user.Id)
-	if user.Id != "" {
+	if user.Id != 0 {
 		err := errors.New("This id is already in use.")
 		fmt.Println(err)
 		return err
@@ -30,7 +32,8 @@ func (UserService) AddUser(id string, pwd string) error {
 	}
 
 	// ユーザの新規登録
-	user = model.User{Id: id, Password: encPwd}
+	int_id, _ := strconv.Atoi(id)
+	user = model.User{Id: int_id, Password: encPwd}
 	stmt, err := Db.Prepare("insert into users (id, enc_pwd) values (?, ?)")
 	if err != nil {
 		fmt.Println("Prepare error")
@@ -76,4 +79,22 @@ func (UserService) GetUserById(id string) (model.User, error) {
 		return user, err
 	}
 	return user, nil
+}
+
+// 既存ユーザに対応したセッションを作成
+func (UserService) CreateSession(id string) (session model.Session, err error) {
+	stmt, err := Db.Prepare("insert into sessions (id, uuid) values (?, ?)")
+	if err != nil {
+		return
+	}
+	defer Db.Close()
+
+	uuid := uuid.New()
+	err = stmt.QueryRow(id, uuid.String()).Scan(&session.Id, &session.Uuid)
+	return
+}
+
+// 既存ユーザに対応したセッションを取得
+func (UserService) GetSession(model.Session, error) {
+
 }
