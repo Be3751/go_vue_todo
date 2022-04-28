831ccc0a48ddb99e1b2555c64e37717115ebbcf2
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-04-28 23:09:31 +0900

    made access control depending on whether to have session-cookie

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -2,13 +2,21 @@
   <v-app-bar absolute color="black" dark app>
     <v-toolbar-title>Task Management!</v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-btn to="/" class="ma-2" color="teal accent-3">
+    <v-btn v-if="sessioned" to="/" class="ma-2" color="teal accent-3">
       <v-icon dark>mdi-format-list-bulleted-square</v-icon>
       View task list
     </v-btn>
-    <v-btn to="/create" class="ma-2" color="teal accent-3">
+    <v-btn v-else to="/login" class="ma-2" color="teal accent-3">
+      <v-icon dark>mdi-format-list-bulleted-square</v-icon>
+      Login
+    </v-btn>
+    <v-btn v-if="sessioned" to="/create" class="ma-2" color="teal accent-3">
       <v-icon dark>mdi-pencil</v-icon>
       Create task
+    </v-btn>
+    <v-btn v-if="sessioned" to="/logout" class="ma-2" color="teal accent-3">
+      <v-icon dark>mdi-pencil</v-icon>
+      Logout
     </v-btn>
   </v-app-bar>
 </template>
@@ -16,7 +24,13 @@
 <script>
   export default {
     data: () => ({
-
+      sessioned: false
     }),
+    mounted() {
+      const sessionId = this.$cookies.get('mysession');
+      if(sessionId != null) {
+        this.sessioned = true
+      }
+    }
   }
 </script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueCookies from 'vue-cookies'
+
 import Signup from '@/views/SignUp.vue'
 import Login from '@/views/LogIn.vue'
 import Logout from '@/views/LogOut.vue'
@@ -10,10 +12,50 @@ import UpdateTask from '@/views/UpdateTask.vue'
 import DeleteTask from '@/views/DeleteTask.vue'
 
 Vue.use(VueRouter)
+Vue.use(VueCookies)
 
-const routes = [
-  {
-    path: '/signup',
+const routes = []
+
+// セッションクッキーの有無でアクセスを制御
+const sessionCookie = Vue.$cookies.get('mysession');
+if(sessionCookie != null) {
+  console.log('Do you want to have a cookie?'); 
+  routes.push(
+    {
+      path: '/',
+      name: 'list',
+      component: TaskList
+    },
+    {
+      path: '/create',
+      name: 'create',
+      component: CreateTask
+    },
+    {
+      path: '/read',
+      name: 'read',
+      component: ReadTask 
+    },
+    {
+      path: '/update/:id',
+      name: 'update',
+      component: UpdateTask 
+    },
+    {
+      path: '/delete/:id',
+      name: 'delete',
+      component: DeleteTask 
+    },
+    {
+      path: '/logout',
+      name: 'logout',
+      component: Logout
+    }
+  );
+} else {
+  console.log('No cookie!');
+  routes.push({
+    path: '/',
     name: 'signup',
     component: Signup
   },
@@ -21,38 +63,9 @@ const routes = [
     path: '/login',
     name: 'login',
     component: Login
-  },
-  {
-    path: '/logout',
-    name: 'logout',
-    component: Logout
-  },
-  {
-    path: '/',
-    name: 'list',
-    component: TaskList
-  },
-  {
-    path: '/create',
-    name: 'create',
-    component: CreateTask
-  },
-  {
-    path: '/read',
-    name: 'read',
-    component: ReadTask 
-  },
-  {
-    path: '/update/:id',
-    name: 'update',
-    component: UpdateTask 
-  },
-  {
-    path: '/delete/:id',
-    name: 'delete',
-    component: DeleteTask 
   }
-]
+  );
+}
 
 const router = new VueRouter({
   mode: 'history',
