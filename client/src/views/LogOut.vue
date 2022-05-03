04dbe3a986c93e573166d8e625941d358de4480f
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-05-03 10:53:20 +0900

    added page and function to logout

--- client/src/views/LogOut.vue
+++ client/src/views/LogOut.vue
@@ -1,27 +1,18 @@
 <template>
     <v-container>
-        <v-row class="mt-2 ml-2">
-        <h1>Make your task!</h1>
+        <v-row class="mt-2 ml-2 mb-5">
+        <h1>Are you sure to log out?</h1>
         </v-row>
         <v-row class="mt-2 ml-2">
-            <v-form ref="create_form">
-                <v-container>
-                    <v-row>
-                        <v-col cols="12" md="12">
-                            <v-text-field v-model="content" :rules="[required, limit_length]" label="something to do" counter=50></v-text-field>
-                        </v-col>
-                        <v-btn class="mr-4" v-on:click="submit">submit</v-btn>
-                        <v-btn v-on:click="clear">clear</v-btn>
-                    </v-row>
-                </v-container>
-            </v-form>
+            <v-btn v-on:click="cancel">Cancel</v-btn>
+            <v-btn class="ml-3 mr-4" v-on:click="logout">Log out</v-btn>
         </v-row>
         <v-row class="mt-7 ml-2">
             <div>
-                <h2 v-if="errored">Sorry. Couldn't create the task!</h2>
+                <h2 v-if="errored">Sorry. Couldn't log out!</h2>
             </div>
             <div>
-                <h2 v-if="succeeded">Successful to create the task!</h2>
+                <h2 v-if="succeeded">Successful to log out!</h2>
             </div>
         </v-row>
     </v-container>
@@ -29,7 +20,24 @@
 
 <script>
 export default {
-
+    name: "LogOut",
+    data: () => ({
+        errored: false,
+        succeeded: false
+    }),
+    methods: {
+        logout() {
+            const sessionKeyName = "mysession"
+            if(this.$cookies.isKey(sessionKeyName)) {
+                this.$cookies.remove(sessionKeyName);
+                console.log("removed session cookie.");
+                this.succeeded = true;
+            } else {
+                console.log("no cookie.");
+                this.errored = true;
+            }
+        }
+    }
 }
 </script>
 
