43952a4b0287c09274e2290fe303fe0e57ccc0e8
薗部匡矢 <msonobe@MacBook-Pro.local>
2022-06-11 09:06:42 +0900

    simplify users table

--- db/sqls/setup.sql
+++ db/sqls/setup.sql
@@ -2,18 +2,13 @@
 
 create table users (
     id varchar(128) primary key,
-    -- uuid varchar(64) unique,
+    uuid varchar(64) not null unique, 
     enc_pwd varchar(255) not null
 );
 
 create table tasks (
     id int primary key,
     content text,
-    user_id int,
+    user_id varchar(128),
     foreign key (user_id) references users(id) 
-);
-
-create table sess (
-    id int primary key,
-    uuid varchar(64) not null unique
 );
